cefc60f7c929436e2ab1452c42e54bfa54dded0f
chushaowei <chu@zjicm.edu.cn>
2016-06-08 10:38:52 +0800

    Initial commit

---
+++ app/src/main/java/com/chushaowei/tpad/testtpad/MainActivity.java
@@ -0,0 +1,20 @@
+package com.chushaowei.tpad.testtpad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //
+        //
+        Toast.makeText(this, "Hello Git", Toast.LENGTH_LONG).show();
+
+    }
+
+}
